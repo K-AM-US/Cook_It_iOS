cd2b10f7f6bfb3cc151594a2084ca6069df2a3dc
mauriciocasillas <kamusgallagher@gmail.com>
2023-12-17 20:55:35 -0600

    New UserDto file

--- Cook_It_iOS/Model/UserDto.swift
+++ Cook_It_iOS/Model/UserDto.swift
@@ -1,8 +1,17 @@
 //
-//  UserDto.swift
+//  UsersDto.swift
 //  Cook_It_iOS
 //
-//  Created by Mauricio Casillas on 17/12/23.
+//  Created by Mauricio Casillas on 13/11/23.
 //
 
 import Foundation
+
+struct UserDto: Codable {
+    let id: String
+    var username: String
+    var first_name: String
+    var last_name: String
+    var recipes: [String]
+    var image: String
+}
